cc5505924f66b5b06fd4c802c1bd9ad8cf0ecdba
Adam <adoantal@gmail.com>
2018-09-16 16:06:48 +0200

    add sub-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,19 @@ import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
